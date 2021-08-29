1cbb057700a44cc27357b42dbe1f12edd91e83c6
Azer Masimov <azer.mesimov@starex.az>
2021-08-29 18:28:15 +0400

    webpack project init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// const lodash = require('lodash');
+
+// const nums = [1, 2, 3, 2, 3, 4, 5, 6, 4];
+
+// const result = lodash.uniq(nums);
+
+// console.log(result);
+
+import post from "./post";
+import user from "./user";
+import "./styles/style.css";
+
+console.log(post);
+console.log(user);
---
+++ src/user.js
@@ -0,0 +1,4 @@
+export default {
+    name: "Azer",
+    age: 24,
+};
